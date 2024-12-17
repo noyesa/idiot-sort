3026352b280a2c4dfb51568f9758556ff98abe9c
Andrew Noyes <noyesa@gmail.com>
2024-12-17 14:44:06 -0800

    Set up basic testing framework

---
+++ src/index.mjs
@@ -0,0 +1,6 @@
+/**
+ * Conducts a sort in the dumbest way possible.
+ * @param {number[]} nums - The numbers to sort.
+ * @returns {number[]} A sorted copy of the input array.
+ */
+export function idiotSort(nums) {}
---
+++ tests/index.test.mjs
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { idiotSort } from "../src/index.mjs";
+
+describe("idiotSort", () => {
+  it("exists", () => {
+    expect(idiotSort).toBeTruthy();
+  });
+});
