f0f7941bfe5128465cbf00b031163670684a160a
Andrew Noyes <noyesa@gmail.com>
2024-12-17 14:54:03 -0800

    Working implementation of the sorting algorithm

--- src/index.mjs
+++ src/index.mjs
@@ -3,4 +3,18 @@
  * @param {number[]} nums - The numbers to sort.
  * @returns {number[]} A sorted copy of the input array.
  */
-export function idiotSort(nums) {}
+export async function idiotSort(nums) {
+  let expectedTimers = nums.length;
+  const output = [];
+
+  return new Promise((acc, rej) => {
+    nums.forEach((num) => {
+      setTimeout(() => {
+        output.push(num);
+        if (--expectedTimers === 0) {
+          acc(output);
+        }
+      }, num);
+    });
+  });
+}
--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -2,7 +2,8 @@ import { describe, it, expect } from "vitest";
 import { idiotSort } from "../src/index.mjs";
 
 describe("idiotSort", () => {
-  it("exists", () => {
-    expect(idiotSort).toBeTruthy();
+  it("sorts the input array", () => {
+    const nums = [1, 7, 2, 6, 3, 5, 4];
+    expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 });
