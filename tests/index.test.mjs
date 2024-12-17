b67d2529c698b7a439cc17a5808db314fa90b2b9
Andrew Noyes <noyesa@gmail.com>
2024-12-17 15:18:25 -0800

    Switched prettier to use singleQuote

--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -1,18 +1,18 @@
-import { describe, it, expect, vi } from "vitest";
-import { idiotSort } from "../src/index.mjs";
+import { idiotSort } from '../src/index.mjs';
+import { describe, it, expect, vi } from 'vitest';
 
-describe("idiotSort", () => {
-  it("sorts the input array", async () => {
+describe('idiotSort', () => {
+  it('sorts the input array', async () => {
     const nums = [1, 7, 2, 6, 3, 5, 4];
     await expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 
-  it("sorts already sorted input", async () => {
+  it('sorts already sorted input', async () => {
     const nums = [1, 2, 3, 4, 5, 6, 7];
     await expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 
-  it("handles empty input", async () => {
+  it('handles empty input', async () => {
     await expect(idiotSort()).resolves.toStrictEqual([]);
     await expect(idiotSort([])).resolves.toStrictEqual([]);
   });
