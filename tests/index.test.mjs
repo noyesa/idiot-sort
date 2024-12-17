0f93236a5de2a49762532d31d9ae3a5c67f8058a
Andrew Noyes <noyesa@gmail.com>
2024-12-17 15:10:50 -0800

    Added awaits to expect().resolves in test

--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -1,19 +1,19 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { idiotSort } from "../src/index.mjs";
 
 describe("idiotSort", () => {
-  it("sorts the input array", () => {
+  it("sorts the input array", async () => {
     const nums = [1, 7, 2, 6, 3, 5, 4];
-    expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
+    await expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 
-  it("sorts already sorted input", () => {
+  it("sorts already sorted input", async () => {
     const nums = [1, 2, 3, 4, 5, 6, 7];
-    expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
+    await expect(idiotSort(nums)).resolves.toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
   });
 
-  it("handles empty input", () => {
-    expect(idiotSort()).resolves.toStrictEqual([]);
-    expect(idiotSort([])).resolves.toStrictEqual([]);
+  it("handles empty input", async () => {
+    await expect(idiotSort()).resolves.toStrictEqual([]);
+    await expect(idiotSort([])).resolves.toStrictEqual([]);
   });
 });
