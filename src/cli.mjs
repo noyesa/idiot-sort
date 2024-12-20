4bf1b825c72268a6b9f2bc9131b8bb56cbc09e42
Andrew Noyes <noyesa@gmail.com>
2024-12-20 01:04:31 -0800

    Migrated test over to CountingMachine

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,38 +1,36 @@
-import ms from 'ms';
-import * as logger from './logger.mjs';
 import random from 'random';
+import * as logger from './logger.mjs';
 import { idiotSort } from './idiot-sort.mjs';
 import { isSorted } from './sort-utils.mjs';
+import { CountingMachine } from './test-runner.mjs';
 
 export function cli() {
-  let length = 0;
-  let runs = 0;
-  let completed = 0;
   let errors = 0;
-
-  while (length < 20) {
-    const input = new Array(length).fill().map(() => random.int(1, 20));
-    const inputAsString = JSON.stringify(input);
+  let completed = 0;
+  const cm = new CountingMachine(
+    [
+      51, // Length of input array to idiotSort, 1-50.
+      19, // Max integer value in generated array, 1-18.
+      11, // Number of test iterations to run, 1-10.
+    ],
+    1,
+  );
+  cm.forEach((length, maxInt) => {
+    const input = new Array(length + 50).fill().map(() =>
+      random.int(
+        1,
+        maxInt + 2, // Start maxInt at 2 so the first pass isn't arrays of 1s.
+      ),
+    );
     const expected = input.toSorted((a, b) => a - b);
-
-    logger.info(`Starting iteration ${length++} with ${inputAsString}`);
-    let n = 0;
-    while (++n <= 100) {
-      // Run the iteration.
-      ++runs;
-      idiotSort(input).then((candidate) => {
-        if (!isSorted(candidate)) {
-          ++errors;
-          logger.error(
-            `Failed sort!\n\tExpected: ${expected}\n\tActual: ${JSON.stringify(candidate)}`,
-          );
-        }
-
-        // This only runs on the final iteration.
-        if (++completed >= 2000) {
-          logger.info(`completed: ${completed} - errors: ${errors}`);
-        }
-      });
-    }
-  }
+    idiotSort(input).then((candidate) => {
+      ++completed;
+      if (!isSorted(candidate)) {
+        ++errors;
+        logger.error(
+          `Failed sort.\n\tExpected: ${expected}\n\tActual: ${candidate}`,
+        );
+      }
+    });
+  });
 }
